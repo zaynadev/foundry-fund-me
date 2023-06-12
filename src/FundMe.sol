709d913923691bf32e54fada7b6716a6cc13bbb0
zaynadev <zayna.oufkir@gmail.com>
2023-06-12 19:54:46 +0100

    add tests for get MINIMUM_USD and owner

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,105 +1,63 @@
 // SPDX-License-Identifier: MIT
-// 1. Pragma
-pragma solidity ^0.8.19;
-// 2. Imports
+pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
-// 3. Interfaces, Libraries, Contracts
-error FundMe__NotOwner();
+error NotOwner();
 
-/**
- * @title A sample Funding Contract
- * @dev This implements price feeds as our library
- */
 contract FundMe {
-    // Type Declarations
     using PriceConverter for uint256;
 
-    // State variables
-    uint256 public constant MINIMUM_USD = 50 * 10 ** 18;
-    address private immutable i_owner;
-    address[] private s_funders;
-    mapping(address => uint256) private s_addressToAmountFunded;
-    AggregatorV3Interface private s_priceFeed;
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
 
-    // Modifiers
-    modifier onlyOwner() {
-        // require(msg.sender == i_owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
-        _;
-    }
-
-    constructor(address priceFeed) {
-        s_priceFeed = AggregatorV3Interface(priceFeed);
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    
+    constructor() {
         i_owner = msg.sender;
     }
 
-    /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
-        require(
-            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
-            "You need to spend more ETH!"
-        );
-        s_addressToAmountFunded[msg.sender] += msg.value;
-        s_funders.push(msg.sender);
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
     }
-
+    
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+    
+    modifier onlyOwner {
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+    
     function withdraw() public onlyOwner {
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < s_funders.length;
-            funderIndex++
-        ) {
-            address funder = s_funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
-        }
-        s_funders = new address[](0);
-
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
-        require(success);
-    }
-
-    function cheaperWithdraw() public onlyOwner {
-        address[] memory funders = s_funders;
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
+        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
-            s_addressToAmountFunded[funder] = 0;
+            addressToAmountFunded[funder] = 0;
         }
-        s_funders = new address[](0);
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
-        require(success);
+        funders = new address[](0);
+      
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
 
-    /**
-     * @notice Gets the amount that an address has funded
-     *  @param fundingAddress the address of the funder
-     *  @return the amount funded
-     */
-    function getAddressToAmountFunded(
-        address fundingAddress
-    ) public view returns (uint256) {
-        return s_addressToAmountFunded[fundingAddress];
+
+    fallback() external payable {
+        fund();
     }
 
-    function getVersion() public view returns (uint256) {
-        return s_priceFeed.version();
+    receive() external payable {
+        fund();
     }
 
-    function getFunder(uint256 index) public view returns (address) {
-        return s_funders[index];
-    }
-
-    function getOwner() public view returns (address) {
-        return i_owner;
-    }
-
-    function getPriceFeed() public view returns (AggregatorV3Interface) {
-        return s_priceFeed;
-    }
 }
+
+
+
+
---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
